12d689481567d25120bfee918ace43f550ff79a5
GeorgeDaris <george.darisgr@gmail.com>
2025-07-05 12:08:43 +0200

    text-wrap pretty for webkit browsers

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -1,33 +1,46 @@
 <template>
   <Teleport to="body">
     <Transition name="cookie" appear>
-      <section v-if="localOpen" aria-labelledby="cookie_heading" class="default-card default-card-content">
+      <section
+        v-if="localOpen"
+        aria-labelledby="cookie_heading"
+        class="default-card default-card-content"
+      >
         <h3 id="cookie-heading">Αποδοχή Cookies</h3>
-          <p>Χρησιμοποιούμε cookies και παρόμοιες τεχνολογίες για να δώσουμε τη δυνατότητα σε υπηρεσίες και λειτουργίες στον ιστότοπό μας και να κατανοήσουμε την αλληλεπίδρασή σας με την υπηρεσία μας. Κάνοντας κλικ στο Αποδοχή, αποδέχεστε τη χρήση τέτοιων τεχνολογιών για το μάρκετινγκ και αναλυτικά στοιχεία.</p>
-          <div>
-            <MainButton size="small">
-              <button @click="acceptCookies">Αποδοχή</button>
-            </MainButton>
-            <NuxtLink to="/privacy-policy">Μάθετε περισσότερα</NuxtLink>
-          </div>
+        <p>
+          Χρησιμοποιούμε cookies και παρόμοιες τεχνολογίες για να δώσουμε τη
+          δυνατότητα σε υπηρεσίες και λειτουργίες στον ιστότοπό μας και να
+          κατανοήσουμε την αλληλεπίδρασή σας με την υπηρεσία μας. Κάνοντας κλικ
+          στο Αποδοχή, αποδέχεστε τη χρήση τέτοιων τεχνολογιών για το μάρκετινγκ
+          και αναλυτικά στοιχεία.
+        </p>
+        <div>
+          <MainButton size="small">
+            <button @click="acceptCookies">Αποδοχή</button>
+          </MainButton>
+          <NuxtLink to="/privacy-policy">Μάθετε περισσότερα</NuxtLink>
+        </div>
       </section>
     </Transition>
   </Teleport>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{ open: boolean }>()
-const emit = defineEmits(['update:open'])
+const props = defineProps<{ open: boolean }>();
+const emit = defineEmits(["update:open"]);
 
-watch(() => props.open, (val) => {
-  localOpen.value = val
-})
+watch(
+  () => props.open,
+  (val) => {
+    localOpen.value = val;
+  }
+);
 
-const localOpen = ref(props.open)
+const localOpen = ref(props.open);
 
 watch(localOpen, (val) => {
-  if (!val) emit('update:open', false)
-})
+  if (!val) emit("update:open", false);
+});
 
 function acceptCookies() {
   // localStorage.setItem('cookieBannerAccepted', 'true')
@@ -37,7 +50,6 @@ function acceptCookies() {
 
 <style scoped>
 section {
-  /* font-size: 0.8rem; */
   transform: scale(0.8);
   position: fixed;
   bottom: 1rem;
@@ -57,6 +69,13 @@ section {
   p {
     max-height: 7rem;
     overflow-y: scroll;
+    mask-image: linear-gradient(
+      180deg,
+      transparent 0%,
+      #000 10%,
+      #000 90%,
+      transparent 100%
+    );
   }
 
   div {
@@ -83,17 +102,14 @@ section {
   }
 }
 
-
 .cookie-enter-from,
 .cookie-leave-to {
   opacity: 0;
   margin-bottom: -2rem;
-
 }
 
 .cookie-enter-active,
 .cookie-leave-active {
   transition: opacity 0.3s, margin-bottom 0.5s;
 }
-
 </style>
